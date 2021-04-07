3f1879d8d12cf6e7b60400fe7c3d923c3c39b87b
Jihirshu Narayan <jihirshu.narayan@gmail.com>
2021-04-07 02:22:37 -0700

    cleaned

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jihirshu/workspace/husky_task1/src
-# Build directory: /home/jihirshu/workspace/husky_task1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("task1_cpp")
-subdirs("task1_python")
